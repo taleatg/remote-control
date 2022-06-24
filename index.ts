13c11e4cbd98b4fd3fd1dd2c013422b2ff72fcfa
taleatg <t_korbyt@mail.ru>
2022-06-24 14:30:10 +0300

    fix: show closing message

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import { webSocketServer } from './src/webSocketServer';
 
 const HTTP_PORT = 3000;
 
-console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+process.stdout.write(`Start static http server on the ${HTTP_PORT} port!\n`);
 
 webSocketServer();
